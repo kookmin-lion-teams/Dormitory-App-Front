7affbb84baeaf78066d2ab87e788a865b3410675
JeongBin Park <obb8923@naver.com>
2024-09-12 14:28:10 +0900

    svg관련

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,11 @@
 const { getDefaultConfig } = require("@expo/metro-config");
-const mergeConfig = require("metro-config").mergeConfig;
+
+const defaultConfig = getDefaultConfig(__dirname);
+const { assetExts, sourceExts } = defaultConfig.resolver;
 
 const config = {
   transformer: {
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
     assetPlugins: ["expo-asset/tools/hashAssetFiles"],
     getTransformOptions: async () => ({
       transform: {
@@ -11,6 +14,16 @@ const config = {
       },
     }),
   },
+  resolver: {
+    assetExts: assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...sourceExts, "svg"],
+  },
 };
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = (async () => {
+  const config = await getDefaultConfig(__dirname);
+  config.resolver.assetExts.push("otf"); // 'otf' 확장자 추가
+  return config;
+})();
+
+// module.exports = { ...defaultConfig, ...config };
