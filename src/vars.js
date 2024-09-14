cdd8492e9848a6481ea05cf4bda31d4ad1ac0c90
JeongBin Park <obb8923@naver.com>
2024-09-14 21:34:38 +0900

    CHORE

--- src/vars.js
+++ src/vars.js
@@ -1,15 +1,15 @@
 import { Dimensions } from "react-native";
 
-// 화면 높이 가져오기
 const { height, width } = Dimensions.get("window");
+
 const vars = {
   primary_color: "#01509F",
   background_color: "#f6f7f9",
+  message_color: "#b3b3b3",
   margin_top: height * 0.03,
   width_90: width * 0.9,
   w_button_radius: 10,
-
   button_radius: 6,
-  message_color: "#b3b3b3",
 };
+
 export default vars;
